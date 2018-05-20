67e18454008887b931e338c8c1e8893cbba04a68
joshi.sujit020386@gmail.com <ILGitHub2>
2018-05-20 18:52:25 -0400

    Trying our New type pattern mentioned here https://doc.rust-lang.org/book/second-edition/ch19-03-advanced-traits.html

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,18 @@
-pub struct Client {
-    port: u32;
-    ip: String;
+extern crate websocket;
+
+use websocket::client::sync::Client;
+use websocket::stream::sync::TcpStream;
+use websocket::stream::Stream;
+use std::ops::Deref;
+
+#[derive(PartialEq, Eq, Hash)]
+pub struct WrapperClient(Client<TcpStream>);
+
+
+impl<C> Deref for WrapperClient {
+    type Target = C;
+
+    fn deref(&self) -> &C {
+        &self.0
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,55 @@
+extern crate websocket;
 mod MyServer;
 
-extern crate websocket;
-
+use websocket::client::sync::Client;
+use websocket::stream::sync::TcpStream;
+use websocket::stream::Stream;
+use websocket::stream::sync::AsTcpStream;
+use std::ops::Deref;
 use std::thread;
 use websocket::OwnedMessage;
 use websocket::sync::Server;
 
+//#[derive(PartialEq, Eq, Hash)]
+pub struct WrapperClient<S>(Client<S>) where S: AsTcpStream + Stream;
+
+/*pub struct ChatClient<S> where S: AsTcpStream + Stream {
+    username: String,
+    wrapper_client: WrapperClient<Client<S>>
+}*/
+
+impl<S> WrapperClient<S> where S: AsTcpStream + Stream{
+    fn new(x: Client<S>) -> WrapperClient<S> {
+        WrapperClient(x)
+    }
+}
+
+impl<S> Deref for WrapperClient<S> where S: AsTcpStream + Stream {
+    type Target = Client<S>;
+
+    fn deref(&self) -> &Client<S> {
+        &self.0
+    }
+}
+
+/*impl<S> PartialEq for WrapperClient<S> where S: AsTcpStream + Stream {
+    fn eq(&self, other: &WrapperClient<S>) -> bool {
+        let o = *other;
+        let s = *self;
+        //let last_ip = last_client.peer_addr().unwrap();
+        //assert_eq!("127.0.0.1:8080".parse(), Ok(socket));
+
+        let otherIp = o.peer_addr().unwrap();
+        let selfIp = s.peer_addr().unwrap();
+
+        otherIp.par
+    }
+}
+impl Eq for Book {}*/
+
 fn main() {
     let server = Server::bind("127.0.0.1:2794").unwrap();
-    let mut chat_server = MyServer::ChatServer{my_users: Vec::new()};
+    //let mut chat_server = MyServer::ChatServer{my_users: Vec::new()};
 
     for request in server.filter_map(Result::ok) {
 
@@ -18,15 +59,7 @@ fn main() {
         }
 
         let mut client = request.use_protocol("rust-websocket").accept().unwrap();
-        chat_server.add_client(client);
-        chat_server.send_message_to_last();
+        let wc = WrapperClient(client);
+
     }
 }
-
-
-// fn main() {
-//     // let mut users = MyServer::ChatServer{my_users: vec![String::from("")]};
-//     let mut users = MyServer::ChatServer{my_users: Vec::new()};
-//     users.add_user(String::from("Morgan"));
-//     println!("First user is {}", users.my_users[0]);
-// }
