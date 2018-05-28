15599c9371df21c286b5bd1b97cecfebc8c096aa
Sujit Joshi <joshi.sujit020386@gmail.com>
2018-05-28 12:51:55 -0400

    1) Adding username (sent in custom header) to our server map.

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,37 @@ mod client;
 
 use websocket::sync::Server;
 use std::collections::HashMap;
-
+use websocket::header::Cookie;
 
 fn main() {
     let server = Server::bind("127.0.0.1:2794").unwrap();
     let mut clients = server::ChatServer{clients: HashMap::new()};
 
-    for request in server.filter_map(Result::ok) {
+    for ws_upgrade in server.filter_map(Result::ok) {
 
-        if !request.protocols().contains(&"rust-websocket".to_string()) {
-            request.reject().unwrap();
+        if !ws_upgrade.protocols().contains(&"rust-websocket".to_string()) {
+            ws_upgrade.reject().unwrap();
             return;
         }
 
-        let mut c = request.use_protocol("rust-websocket").accept().unwrap();
-        let wc2 = client::WrapperClient::new(String::from("Sujit"), c);
+        let h = ws_upgrade.request.headers.clone();
+        let c = ws_upgrade.use_protocol("rust-websocket").accept().unwrap();
+        let user_id: String;
+        {
+            //let headrs = c.headers();
+            let cookie: &Cookie = match h.get() {
+                Some(c) => c,
+                None => {
+                    println!("Unable to determine user_id");
+                    continue;
+                }
+            };
+            let v = &*cookie;
+            let id = v[0].clone();
+            user_id = id.split('=').collect::<Vec<&str>>().get(1).unwrap().to_string();
+        }
+        print!("User id is {}", user_id);
+        let wc2 = client::WrapperClient::new(user_id, c);
         clients.add_client(wc2);
     }
 }
